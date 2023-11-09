9248679174097ebc6926709d4b7ede729fd387fc
Kishor Vinod <117641547+kishorvinod@users.noreply.github.com>
2023-11-09 11:45:58 +0530

    Add files via upload

--- cycle2/Question1/MainActivity.java
+++ cycle2/Question1/MainActivity.java
@@ -1,45 +1,110 @@
-package com.example.sjcet.sharedpreference;
+package com.example.sjcet.calculator;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private EditText usernameEditText;
-    private EditText emailEditText;
-    private EditText passwordEditText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
+    private String currentInput = "";
+    private String operator = "";
+    private double firstOperand = 0;
+    private double secondOperand = 0;
+    private boolean isNewInput = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        usernameEditText = findViewById(R.id.usernameEditText);
-        emailEditText = findViewById(R.id.emailEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
+        textView = findViewById(R.id.textView);
+        GridLayout gridLayout = findViewById(R.id.gridLayout);
+        String[] buttonLabels = {
+                "7", "8", "9", "/",
+                "4", "5", "6", "*",
+                "1", "2", "3", "-",
+                "C", "0", "=", "+"
+        };
+        for (String label : buttonLabels) {
+            Button button = new Button(this);
+            button.setText(label);
+            button.setTextSize(24);
+            button.setOnClickListener(this);
+            gridLayout.addView(button);
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String buttonText = button.getText().toString();
+        switch (buttonText) {
+            case "=":
+                calculateResult();
+                break;
+            case "C":
+                clearInput();
+                break;
+            default:
+                handleInput(buttonText);
+                break;
+        }
+    }
+    private void handleInput(String input) {
+        if (isNewInput) {
+            currentInput = input;
+            isNewInput = false;
+        } else {
+            currentInput += input;
+        }
+        updateDisplay();
+    }
+    private void clearInput() {
+        currentInput = "";
+        operator = "";
+        firstOperand = 0;
+        secondOperand = 0;
+        isNewInput = true;
+        updateDisplay();
+    }
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void calculateResult() {
+        if (!isNewInput) {
+            String expression = currentInput;
+            try {
+                // Use JavaScript eval() to evaluate the expression
+                WebView webView = new WebView(this);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.addJavascriptInterface(new Object() {
+                    @JavascriptInterface
+                    public void processHTML(String html) {
+                        // Process the result returned from JavaScript
+                        currentInput = html;
+                        isNewInput = true;
+                        updateDisplay();
+                    }
+                }, "Android");
+                webView.evaluateJavascript("javascript:Android.processHTML(eval('" + expression + "'))", null);
+            } catch (Exception e) {
+                currentInput = "Error: Invalid expression";
+                isNewInput = true;
+                updateDisplay();
+            }
+        }
     }
 
-    public void register(View view) {
-        // Get user input from EditText fields
-        String username = usernameEditText.getText().toString();
-        String email = emailEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
-
-        // Store registration details in SharedPreferences
-        SharedPreferences preferences = getSharedPreferences("UserData", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("username", username);
-        editor.putString("email", email);
-        editor.putString("password", password);
-        editor.apply();
-
-        // Redirect to the main activity or login screen using an Intent
-        Intent intent = new Intent(this, Main2Activity.class);
-
-        // Start the MainActivity
-        startActivity(intent);
+    /**
+     *
+     */
+    private void updateDisplay() {
+        textView.setText(currentInput);
     }
 }
